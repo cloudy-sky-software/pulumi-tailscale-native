f06e35a571e989d197b9a2d8369bd29f6af2ee5c
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2023-03-25 12:01:28 -0700

    Revert codegen target folder name

--- provider/cmd/pulumi-gen-tailscale/main.go
+++ provider/cmd/pulumi-gen-tailscale/main.go
@@ -88,7 +88,7 @@ func main() {
 	case Schema:
 		openapiDoc := openapi.GetOpenAPISpec(openapiDocBytes)
 		schemaSpec, metadata := providerSchemaGen.PulumiSchema(*openapiDoc)
-		providerDir := filepath.Join(".", "provider", "cmd", "pulumi-resource-tailscale-native")
+		providerDir := filepath.Join(".", "provider", "cmd", "pulumi-resource-tailscale")
 		mustWritePulumiSchema(schemaSpec, providerDir)
 
 		// Write the metadata.json file as well.
