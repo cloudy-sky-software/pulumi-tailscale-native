11e64c1e0e2081f8920e6ceb2948268cb92a9d61
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2023-03-25 12:13:53 -0700

    Revert "Revert codegen target folder name"
    This reverts commit f06e35a571e989d197b9a2d8369bd29f6af2ee5c.

--- provider/cmd/pulumi-gen-tailscale/main.go
+++ provider/cmd/pulumi-gen-tailscale/main.go
@@ -88,7 +88,7 @@ func main() {
 	case Schema:
 		openapiDoc := openapi.GetOpenAPISpec(openapiDocBytes)
 		schemaSpec, metadata := providerSchemaGen.PulumiSchema(*openapiDoc)
-		providerDir := filepath.Join(".", "provider", "cmd", "pulumi-resource-tailscale")
+		providerDir := filepath.Join(".", "provider", "cmd", "pulumi-resource-tailscale-native")
 		mustWritePulumiSchema(schemaSpec, providerDir)
 
 		// Write the metadata.json file as well.
