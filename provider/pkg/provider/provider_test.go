b66f9e4f609953f3fa7ad9b8948ff87c28fafd41
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2023-04-22 09:45:37 -0700

    Update the provider f/w lib to the latest (#50)
    * Update the provider f/w lib to the latest. Re-enable tests.
    
    * Fix test

--- provider/pkg/provider/provider_test.go
+++ provider/pkg/provider/provider_test.go
@@ -20,24 +20,18 @@ import (
 )
 
 const testCreateJSONPayload = `{
-    "autoDeploy": "yes",
-    "branch": "master",
-    "envVars": [{ "key": "PORT", "value": "8080" }],
-    "name": "An Express.js web service",
-    "ownerId": "usr-somefakeownerid",
-    "repo": "https://github.com/render-examples/express-hello-world",
-    "serviceDetails": {
-        "env": "node",
-        "envSpecificDetails": {
-            "buildCommand": "yarn",
-            "startCommand": "node app.js"
-        },
-        "numInstances": 1,
-        "plan": "starter",
-        "pullRequestPreviewsEnabled": "no",
-        "region": "oregon"
-    },
-    "type": "web_service"
+    "capabilities": {
+		"devices": {
+			"create": {
+				"ephemeral": true,
+				"preauthorized": true,
+				"reusable": true,
+				"tags": ["admin:authorized"]
+			}
+		}
+	},
+	"expirySeconds": 300,
+	"tailnet": "mytailnet@tailscale.com"
 }
 `
 
@@ -63,14 +57,14 @@ func makeTestProvider(ctx context.Context, t *testing.T) pulumirpc.ResourceProvi
 	pSchemaBytes := readFileFromProviderResourceDir(t, "schema.json")
 	metadataBytes := readFileFromProviderResourceDir(t, "metadata.json")
 
-	p, err := makeProvider(nil, "", "", pSchemaBytes, openapiBytes, metadataBytes)
+	p, err := makeProvider(nil, "tailscale-native", "", pSchemaBytes, openapiBytes, metadataBytes)
 
 	if err != nil {
 		t.Fatalf("Could not create a provider instance: %v", err)
 	}
 
 	_, err = p.Configure(ctx, &pulumirpc.ConfigureRequest{
-		Variables: map[string]string{"tailscale:config:apiKey": "fakeapikey"},
+		Variables: map[string]string{"tailscale-native:config:apiKey": "fakeapikey"},
 	})
 
 	if err != nil {
@@ -81,36 +75,36 @@ func makeTestProvider(ctx context.Context, t *testing.T) pulumirpc.ResourceProvi
 }
 
 func TestDiff(t *testing.T) {
-	t.Skip("Skipping temporarily. Needs update.")
-
 	ctx := context.Background()
 
 	p := makeTestProvider(ctx, t)
 
-	outputs := make(map[string]interface{})
-	outputs["name"] = "Test"
-	oldsStruct, _ := plugin.MarshalProperties(state.GetResourceState(outputs, resource.NewPropertyMapFromMap(outputs)), state.DefaultMarshalOpts)
+	original := make(map[string]interface{})
+	original["expirySeconds"] = 300
+	outputs := map[string]interface{}{"expirySeconds": 300, "key": "somesecretkey"}
+	oldsStruct, _ := plugin.MarshalProperties(state.GetResourceState(outputs, resource.NewPropertyMapFromMap(original)), state.DefaultMarshalOpts)
 
 	news := make(map[string]interface{})
-	news["name"] = "Test2"
+	// Now update the input property.
+	news["expirySeconds"] = 400
 	newsStruct, _ := plugin.MarshalProperties(resource.NewPropertyMapFromMap(news), state.DefaultMarshalOpts)
 
-	resp, err := p.Diff(ctx, &pulumirpc.DiffRequest{Id: "", Urn: "urn:pulumi:some-stack::some-project::tailscale:services:StaticSite::someResourceName", Olds: oldsStruct, News: newsStruct})
+	resp, err := p.Diff(ctx, &pulumirpc.DiffRequest{Id: "", Urn: "urn:pulumi:some-stack::some-project::tailscale-native:tailnet:Key::myAuthKey", Olds: oldsStruct, News: newsStruct})
 	assert.Nil(t, err)
 	assert.Equal(t, pulumirpc.DiffResponse_DIFF_SOME, resp.Changes)
 	assert.NotEmpty(t, resp.Diffs)
 	assert.Len(t, resp.Diffs, 1)
-	assert.Empty(t, resp.Replaces)
+	// Since the tailscale-native:tailnet:Key resource does not support updates,
+	// we should get a replacement.
+	assert.NotEmpty(t, resp.Replaces)
 }
 
 func TestCreate(t *testing.T) {
-	t.Skip("Skipping temporarily. Needs update.")
-
 	ctx := context.Background()
 
 	var inputs map[string]interface{}
 	if err := json.Unmarshal([]byte(testCreateJSONPayload), &inputs); err != nil {
-		t.Fatal("Failed to unmarshal test payload")
+		t.Fatalf("Failed to unmarshal test payload: %v", err)
 	}
 
 	p := makeTestProvider(ctx, t)
@@ -118,7 +112,7 @@ func TestCreate(t *testing.T) {
 	inputProperties, _ := plugin.MarshalProperties(resource.NewPropertyMapFromMap(inputs), state.DefaultMarshalOpts)
 
 	_, err := p.Create(ctx, &pulumirpc.CreateRequest{
-		Urn:        "urn:pulumi:dev::tailscale-ts::tailscale:services:WebService::webservice",
+		Urn:        "urn:pulumi:dev::tailscale-ts::tailscale-native:tailnet:Key::myAuthKey",
 		Properties: inputProperties,
 	})
 
